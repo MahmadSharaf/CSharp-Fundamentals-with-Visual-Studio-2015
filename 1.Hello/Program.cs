805fa11c3a8c9ece5208e14bbab3a4d84eaf931e
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-03 14:06:21 +0200

    Add project files.

---
+++ 1.Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System; /*main Space to use Console Class*/
+
+namespace _1.Hello 
+{
+    class Program 
+    {
+        static void Main(string[] args) /*args are the input send by the user*/
+        {
+            Console.WriteLine("Hello," + args[0]);
+        }
+    }
+}
