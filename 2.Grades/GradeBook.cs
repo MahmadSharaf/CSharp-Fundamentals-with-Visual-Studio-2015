d6a5940ed3e10eafcc13af5f4c6468fc50bedcc1
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-03 15:20:09 +0200

    Initial Grade program files

---
+++ 2.Grades/GradeBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic; /* This name space full of collections classes that allows to store multiole things such as list collection*/
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Grades
+{
+    class GradeBook
+    {
+    }
+}
