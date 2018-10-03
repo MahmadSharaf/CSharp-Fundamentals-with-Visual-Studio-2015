4c49261a249dff394a75760a5dd884e9adbf2668
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-03 14:53:30 +0200

    Add HoursOfSleep prompt If and Else Convert string to int

--- 1.Hello/Program.cs
+++ 1.Hello/Program.cs
@@ -9,7 +9,17 @@ namespace _1.Hello
         {
             Console.WriteLine("Your Name: ");
             string name = Console.ReadLine(); /*Scans for an input*/
+            Console.WriteLine("How many hours have you slept? ");
+            int hourOfSleep = int.Parse(Console.ReadLine()); /*Readline expects strings so it needed to be converted explicity*/
             Console.WriteLine("Hello," + name); /*Prints out*/
+            if (hourOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested.");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep.");
+            }
         }
     }
 }
